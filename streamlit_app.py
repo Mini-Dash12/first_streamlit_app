f581861cd5e411d97bf2f4894def9b572503dffe
Mini Dash <104824806+Mini-Dash12@users.noreply.github.com>
2022-05-05 07:41:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.header('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
