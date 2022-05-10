40aadf943ac5912ff9a5e83acefdf3cb3d728ed1
Mini Dash <104824806+Mini-Dash12@users.noreply.github.com>
2022-05-10 14:24:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,10 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('fruityvice Fruit Advice!')
 Fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered',fruit_choice)
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.write('The user entered',fruit_choice)
+
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
