2e6fd756c09bd04a30a28dffd79eb6b6d6560ce1
Mini Dash <104824806+Mini-Dash12@users.noreply.github.com>
2022-05-10 13:53:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 import requests
-fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.dataframe(fruityvice_response)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
